17ca0b97d76f20ec72a110ca898f2c0ac29d5837
Vitali Rebkavets <REVIMI@users.noreply.github.com>
2021-11-24 20:49:54 +0300

    review 1

--- square.py
+++ square.py
@@ -1,13 +1,13 @@
-
+  # todo remove blank line
 def square(x):
     result = ((x*4), (x**2), (x*(2**(1/2))))
-    print (type(result))
+    print (type(result))  # todo don't print inside function
 
     print("Периметр = ", result[0],'\n'
           "Площадь = ", result[1],'\n'
           "Диагональ = ", result[2])
 
-
+## todo too many blank lines
 
 
 square(int(input("Сторона квадрата равна ")))
