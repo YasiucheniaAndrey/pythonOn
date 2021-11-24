aa37f84b23e8ef1412023ac524f09d7c1c9ca8a3
Vitali Rebkavets <REVIMI@users.noreply.github.com>
2021-11-24 20:53:19 +0300

    review 1

--- visYear.py
+++ visYear.py
@@ -1,11 +1,13 @@
+# todo please use snake_case for file names in python (ex. `leap_year.py`)
 def is_year_leap(year):
     z = year % 4
 
     if z == 0:
-        print("True")
+        print("True")  # todo don't print inside function. It should return something and then you may use it to output for the user
     else:
         print("False")
 
 a = int(input('введите год пжлста '))
 
+# todo add condition like here https://github.com/YasiucheniaAndrey/pythonOn/blob/main/main.py#L13
 is_year_leap(a)
