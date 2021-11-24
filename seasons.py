101110aa03c5f3131f5fad060f771b8693c082ef
Vitali Rebkavets <REVIMI@users.noreply.github.com>
2021-11-24 20:47:06 +0300

    review 1

--- seasons.py
+++ seasons.py
@@ -1,7 +1,7 @@
-def season (month):
+def season (month):  # remove whitespace `def season(month):`
 
-    if 0<month<2 or month==12:
-        print('Now is winter')
+    if 0<month<2 or month==12:  # add whitespaces `if 0 < month < 2 or month == 12:`
+        print('Now is winter')  # use return instead print
     elif 3<=month<6:
         print('Now is spring')
     elif 6<=month<9:
@@ -13,9 +13,9 @@ def season (month):
 
 
 
+# todo too many blank lines
 
 
 
-
-
+# todo add a condition https://github.com/YasiucheniaAndrey/pythonOn/blob/main/main.py#L13
 season(int(input("Month please ")))
